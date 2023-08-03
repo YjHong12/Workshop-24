74f273a1f8422cfde0c39f15fee98006f48d3cf2
Jennifer <jennifer.y.hong@gmail.com>
2023-08-03 16:40:10 -0400

    select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import './App.css'
-import {puppyList} from './data.js'
-import { useState } from 'react'
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  // console.log("puppyList: ", puppyList);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup)
 
   return (
-      <div className = "App"> 
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}
-          </p>;
-        })
-      }
-
-      </div>
-
+    <div className="App">
+        {featPupId && (
+    <div>
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+    </div>
+  )}
+      {puppies.map((puppy) => {
+        return (
+          <button onClick={() => {setFeatPupId(puppy.id) }} key={puppy.id}>
+            {" "}
+            {puppy.name}
+          </button>
+        );
+      })}
+    </div>
   );
 }
 
-export default App
+export default App;
